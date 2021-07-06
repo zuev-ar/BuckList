031681753fe98a76d22211a01b6fa9a4139b37d4
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-07-06 23:30:22 +0500

    project 14, part 1, Writing data to the documents directory

--- BuckList/ContentView.swift
+++ BuckList/ContentView.swift
@@ -8,35 +8,28 @@
 import SwiftUI
 import CoreData
 
-struct User: Identifiable, Comparable {
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-    
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-    
-    static func > (lhs: User, rhs: User) -> Bool {
-        lhs.lastName > rhs.lastName
-    }
-}
-
 struct ContentView: View {
-    let values = [1, 5, 3, 6, 2 ,9].sorted()
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Kristine", lastName: "Kochanski"),
-        User(firstName: "David", lastName: "Lister")
-    ].sorted()
     
     var body: some View {
-        List(values, id: \.self) {
-            Text(String($0))
-        }
-        List(users) { user in
-            Text(" \(user.firstName), \(user.lastName)")
-        }
+        Text("Hello world")
+            .onTapGesture {
+                let str = "Test Message"
+                let url = self.getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return paths[0]
     }
 }
 
