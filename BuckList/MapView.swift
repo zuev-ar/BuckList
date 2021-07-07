2883847285eaf1eb45b5e55c55ad7ee53354e817
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-07-07 19:23:29 +0500

    project 14, part 2, Communicating with a MapKit coordinator

--- BuckList/MapView.swift
+++ BuckList/MapView.swift
@@ -9,8 +9,40 @@ import MapKit
 import SwiftUI
 
 struct MapView: UIViewRepresentable {
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            view.canShowCallout = true
+            return view
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            
+        }
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     func makeUIView(context: Context) -> some UIView {
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "London"
+        annotation.subtitle = "Capital of England"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: 0.13)
+        
+        mapView.addAnnotation(annotation)
+        
         return mapView
     }
     
